ed40ae4c2062d836032458512ec7b6f0d0656152
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-01-29 21:59:57 +0700

    membuat variabel

--- lib/app/modules/bottom_navigation/views/bottom_navigation_view.dart
+++ lib/app/modules/bottom_navigation/views/bottom_navigation_view.dart
@@ -1,3 +1,5 @@
+import 'package:final_project_2023/app/modules/home/views/home_view.dart';
+import 'package:final_project_2023/app/modules/lapor_page/views/lapor_page_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -5,20 +7,18 @@ import 'package:get/get.dart';
 import '../controllers/bottom_navigation_controller.dart';
 
 class BottomNavigationView extends GetView<BottomNavigationController> {
-  const BottomNavigationView({Key? key}) : super(key: key);
+  BottomNavigationController bottomNavigationController =
+      Get.put(BottomNavigationController());
+  BottomNavigationView({Key? key}) : super(key: key);
+
+  final screens = [
+    const HomeView(),
+    const LaporPageView(),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'BottomNavigationView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
     );
   }
 }
