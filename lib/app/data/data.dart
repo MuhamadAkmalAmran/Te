746a719ec056cf597abecab2a1ff5aa8cb7080c4
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-01-29 21:09:14 +0700

    membuat data class

---
+++ lib/app/data/data.dart
@@ -0,0 +1,11 @@
+class SeputarInfo {
+  final int position;
+  final String name;
+  final String description;
+
+  SeputarInfo(
+    this.position, {
+    required this.name,
+    required this.description,
+  });
+}
