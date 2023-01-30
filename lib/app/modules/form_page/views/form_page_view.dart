35a921c7bf08f9fb488d3c21d6c871dc37bb0b07
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-01-30 20:46:42 +0700

    menambahkan key pada form

--- lib/app/modules/form_page/views/form_page_view.dart
+++ lib/app/modules/form_page/views/form_page_view.dart
@@ -14,6 +14,7 @@ class FormPageView extends GetView<FormPageController> {
         centerTitle: true,
       ),
       body: Form(
+        key: controller.loginFormKey,
         child: Column(
           children: [
             Padding(
