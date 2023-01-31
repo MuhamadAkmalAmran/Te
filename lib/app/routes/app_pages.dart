47f01a257e3fe1043d140a5bfeedb409fa9e1fc0
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-01-31 11:31:08 +0700

    merubah route page

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -18,7 +18,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.BOTTOM_NAVIGATION;
+  static const INITIAL = Routes.LOGIN_PAGE;
 
   static final routes = [
     GetPage(
