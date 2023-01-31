7c6c0f31313b431547ba2b2be447c93e29d10182
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-01-31 12:12:23 +0700

    membuat tampilan ui login page

--- lib/app/modules/login_page/views/login_page_view.dart
+++ lib/app/modules/login_page/views/login_page_view.dart
@@ -1,3 +1,4 @@
+import 'package:final_project_2023/app/data/warna.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -9,14 +10,154 @@ class LoginPageView extends GetView<LoginPageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginPageView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginPageView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [gradientStartColor, gradientEndColor],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.3, 0.7],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Icon(
+                Icons.forest,
+                size: 100,
+                color: Colors.white,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 480,
+                width: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "HELLO",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Selamat Datang di Aplikasi Silampah",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Silahkan Login utnuk masuk ke aplikasi",
+                      style: TextStyle(fontSize: 15, color: Colors.grey),
+                    ),
+                    SizedBox(height: 40,),
+                    Container(
+                      width: 400,
+                      child: Form(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: TextFormField(
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600),
+                                decoration: InputDecoration(
+                                  suffixIcon: Icon(Icons.email_outlined),
+                                  label: const Text('Email'),
+                                  hintText: 'Isi Email',
+                                  border: OutlineInputBorder(),
+                                ),
+                                onChanged: (newValue) {},
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: TextFormField(
+                                obscureText: true,
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600),
+                                decoration: InputDecoration(
+                                  suffixIcon: Icon(Icons.password),
+                                  label: const Text('Password'),
+                                  hintText: 'Isi Password',
+                                  border: OutlineInputBorder(),
+                                ),
+                                onChanged: (newValue) {},
+                              ),
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                GestureDetector(
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    width: 250,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      gradient: LinearGradient(
+                                        colors: [
+                                          gradientStartColor,
+                                          gradientEndColor
+                                        ],
+                                        begin: Alignment.centerLeft,
+                                        end: Alignment.centerRight,
+                                        stops: [0.3, 0.7],
+                                      ),
+                                    ),
+                                    child: Padding(
+                                      padding: EdgeInsets.all(12.0),
+                                      child: Text(
+                                        "Login",
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                  onTap: () {},
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Belum Punya Akun?"),
+                                    TextButton(
+                                      onPressed: () {},
+                                      child: Text("Register Sekarang"),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
