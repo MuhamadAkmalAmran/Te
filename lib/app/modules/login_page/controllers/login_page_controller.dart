12a8707f37d6df3f22ee0244bbd1f69126bfff5d
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-01-31 12:14:53 +0700

    membuat variabel pada login page controller

--- lib/app/modules/login_page/controllers/login_page_controller.dart
+++ lib/app/modules/login_page/controllers/login_page_controller.dart
@@ -1,23 +1,28 @@
+import 'package:firedart/firedart.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginPageController extends GetxController {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GlobalKey<FormState>loginFormKey = GlobalKey<FormState>();
+  
   //TODO: Implement LoginPageController
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
+  // final count = 0.obs;
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  // }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  // @override
+  // void onReady() {
+  //   super.onReady();
+  // }
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+  // @override
+  // void onClose() {
+  //   super.onClose();
+  // }
 
-  void increment() => count.value++;
+  // void increment() => count.value++;
 }
