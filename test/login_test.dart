68eb79e4467419a93d08233a1cf9e2e2423586e6
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2023-11-15 22:27:40 +0700

    add login_test for unit testing

---
+++ test/login_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  test('Valid email and password', () {
+    expect(validateIdAndPassword('akmal@gmail.com', '12345678'), isTrue);
+  });
+}
+
+bool validateIdAndPassword(String id, String password) {
+  if (id.isEmpty || password.isEmpty) {
+    return false;
+  }
+  if (id.contains(' ')) {
+    return false;
+  }
+
+  if (password.length < 6 || password.length > 16) {
+    return false;
+  }
+
+  return true;
+}
